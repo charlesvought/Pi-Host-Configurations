c913a004e3c0bafd3250d09e94f007ddaf3afd49
Charles Vought <charlesvought@users.noreply.github.com>
2021-05-15 11:51:30 -0400

    Update PiHostRun-windows10.ps1

--- Windows10/PiHostRun-windows10.ps1
+++ Windows10/PiHostRun-windows10.ps1
@@ -1,12 +1,18 @@
 ﻿# Set path to log output to users' desktop
 $UserPath = "$($env:USERPROFILE)\Desktop\PiHoleDNS-Output.txt"
+#Start Transcripting
 Start-Transcript -Append -Path $UserPath
 
 Write-Host $PSCommandPath
 Write-Host "# Existing DNS Settings..."
 Get-DnsClientServerAddress
 
-Set-DnsClientServerAddress -InterfaceIndex 14 -ResetServerAddresses
+# Set new Pi-Hole DNS settings
+$AllNetAdapters=Get-NetAdapter
+foreach ($Adapter in $AllNetAdapters)
+{
+    Set-DnsClientServerAddress -InterfaceIndex $Adapter.ifIndex -Validate -ServerAddresses ("8.8.8.8","8.8.4.4")
+}
 
 Write-Host "`r`n# New DNS Settings..."
 Get-DnsClientServerAddress
