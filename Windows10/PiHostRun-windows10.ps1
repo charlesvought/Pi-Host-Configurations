29f3570652d5e92af78c94c53c0407b0f92633ac
Charles Vought <charlesvought@users.noreply.github.com>
2021-05-15 10:55:45 -0400

    Update PiHostRun-windows10.ps1

--- Windows10/PiHostRun-windows10.ps1
+++ Windows10/PiHostRun-windows10.ps1
@@ -1,20 +1,9 @@
-﻿#Declaring Variables
-#$var1 = "x.x.x.x"
-#$var2 = "x.x.x.x"
-#$Adapter1 = Get-NetAdapter * | select -expand name
-
-#Assigning primary and secondary DNS servers
-#netsh interface ipv4 set dnsservers name=$Adapter1 static $var1 primary
-#netsh interface ipv4 add dnsservers name=$Adapter1 $var2 index=2
-#Write-Host "The Primary DNS is:" $var1
-#Write-Host "The Secondary DNS is:" $var2
-#exit
-
+﻿
 # Ask for elevated permissions if required
-#If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-#	Start-Process powershell.exe "-noexit -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-#}
-#Set path to log output to users' desktop
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	Start-Process powershell.exe "-noexit -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+}
+# Set path to log output to users' desktop
 $UserPath = "$($env:USERPROFILE)\Desktop\PiHoleDNS-Output.txt"
 Start-Transcript -Append -Path $UserPath
 
@@ -30,5 +19,5 @@ Write-Host "`r`n# Clearing DNS Cache...`r`n"
 Clear-DnsClientCache
 Write-Host "# All operations completed.`r`n"
 Write-Host "# Display IP Configurations"
-#Display IP Config Information
+# Display IP Config Information
 ipconfig /all
