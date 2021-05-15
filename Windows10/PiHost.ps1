9366d9efa05931e33330b666fbe2d9eeded8437c
Charles Vought <charlesvought@users.noreply.github.com>
2021-05-15 16:20:20 -0400

    Create PiHost.ps1

---
+++ Windows10/PiHost.ps1
@@ -0,0 +1,6 @@
+﻿If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -windowstyle hidden -Verb RunAs
+} else {
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/charlesvought/Pi-Host-Configurations/main/Windows10/PiHostRun-windows10.ps1'))
+}
