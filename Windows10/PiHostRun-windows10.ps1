579a85665e29334aaa5a81d74f64ce257b7fb742
Charles Vought <charlesvought@users.noreply.github.com>
2021-05-15 11:09:51 -0400

    Update PiHostRun-windows10.ps1

--- Windows10/PiHostRun-windows10.ps1
+++ Windows10/PiHostRun-windows10.ps1
@@ -1,8 +1,9 @@
-﻿
-# Ask for elevated permissions if required
+﻿# Ask for elevated permissions if required
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-noexit -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
 }
+
+Write-Host $PSCommandPath
 # Set path to log output to users' desktop
 $UserPath = "$($env:USERPROFILE)\Desktop\PiHoleDNS-Output.txt"
 Start-Transcript -Append -Path $UserPath
